1b6bc537b00419f1e4dbddb98137157fa4219d13
Emerson Freitas <mersonsi@gmail.com>
2024-01-02 22:22:01 -0300

    fix: add json formatter require

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require_relative "../config/environment"
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
 require "simplecov"
+require "simplecov-json"
 require "sidekiq/testing/inline"
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
